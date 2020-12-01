eb62b48509c51c854741d8928eb8e93fa08cc1d1
RLShaw <rshaw@certifiedmixtapez.com>
2020-11-30 19:52:55 -0600

    Initial commit

---
+++ CMTZ-API/Startup.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.S3;
+using Imageflow.Server;
+using Imageflow.Server.Storage.S3;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using static Amazon.RegionEndpoint;
+
+namespace DO_Images
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+
+            Configuration = configuration;
+
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            AmazonS3Config clientConfig = new AmazonS3Config();
+            clientConfig.ServiceURL = "https://nyc3.digitaloceanspaces.com";
+            clientConfig.SignatureVersion = "v2";
+            services.AddImageflowS3Service(new S3ServiceOptions(null, null)
+                    .MapPrefix("/do/", clientConfig, "cmtz", "", false, false));
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+
+            app.UseHttpsRedirection();
+
+            app.UseImageflow(new ImageflowMiddlewareOptions()
+                .SetMapWebRoot(false)
+                .SetMyOpenSourceProjectUrl("https://github.com/my/project"));
+
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
